ca36b43229ee936c43fd8bcf685564c51272af48
unknown <kianooshnike1365@gmail.com>
2021-05-17 11:57:27 +0430

    update 16

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Selector } from "./components/Selector";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { List } from "./containers/Products/List";
-import { About } from "./containers/About/About";
-import { Contact } from "./containers/Contact/Contact";
-import { SignIn } from "./containers/SignIn/SignIn";
-import { SignUp } from "./containers/SignUp/SignUp";
+
 export class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/productsList">Products List</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-              <li>
-                <Link to="/signin">Sign In</Link>
-              </li>
-              <li>
-                <Link to="/signup">Sign Up</Link>
-              </li>
-            </ul>
-          </nav>
-          <Route path="/" />
-          <Route path="/productsList" component={List} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/signin" component={SignIn} />
-          <Route path="/signup" component={SignUp} />
-        </div>
-      </Router>
+      <div className="App">
+        <Selector />
+      </div>
     );
   }
 }
--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import styled from "styled-components";
+import { List } from "../containers/Products/List";
+import { About } from "../containers/About/About";
+import { Contact } from "../containers/Contact/Contact";
+import { SignIn } from "../containers/SignIn/SignIn";
+import { SignUp } from "../containers/SignUp/SignUp";
 export class Selector extends Component {
   render() {
     const UL = styled.ul`
@@ -12,21 +17,39 @@ export class Selector extends Component {
       display: inline;
       padding: 5rem;
     `;
-
     return (
-      <div>
-        {/* <Router>
-          <UL>
-            <LI>
-              <Link to="/test" href="/Test">
-                Products
-              </Link>
-
-              <Route path="/test" Component={Test}></Route>
-            </LI>
-          </UL>
-        </Router> */}
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <UL>
+              <LI>
+                <Link to="/">Home</Link>
+              </LI>
+              <LI>
+                <Link to="/productsList">Products List</Link>
+              </LI>
+              <LI>
+                <Link to="/about">About</Link>
+              </LI>
+              <LI>
+                <Link to="/contact">Contact</Link>
+              </LI>
+              <LI>
+                <Link to="/signin">Sign In</Link>
+              </LI>
+              <LI>
+                <Link to="/signup">Sign Up</Link>
+              </LI>
+            </UL>
+          </nav>
+          <Route path="/" />
+          <Route path="/productsList" component={List} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+        </div>
+      </Router>
     );
   }
 }
